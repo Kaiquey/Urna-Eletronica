98df5aba2994b213089839b1923bdaaf27fb2b5b
Kaique.Dev <kaiquesouzacosta16@gmail.com>
2026-09-01 23:09:33 -0300

    Criando atributos da classe

--- Urna/src/Eleitor.java
+++ Urna/src/Eleitor.java
@@ -1,3 +1,12 @@
-public class Eleitor{
+public class Eleitor extends Pessoa{
+    private String tituloEleitor;
+    private boolean voto;
+
+    public Eleitor(String nome, String cpf, Localdate dataNascimento, String tituloEleitor){
+        super(nome, cpf, dataNascimento);
+        this.tituloEleitor = tituloEleitor;
+        this.voto = false;
+    }
+
 
 }
