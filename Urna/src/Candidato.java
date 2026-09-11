d53a58c3ff892dd16a3297599c470aa65557d213
Kaique.Dev <kaiquesouzacosta16@gmail.com>
2026-09-11 14:52:47 -0300

    Criando Construtor, getters e validações do candidato

--- Urna/src/Candidato.java
+++ Urna/src/Candidato.java
@@ -1,3 +1,27 @@
 public class Candidato{
+    private final int id;
+    private final String Partido;
+
+    public Candidato(String nome, String cpf, LocalDate dataNascimento, int id,String Partido) {
+        super(nome,cpf,dataNascimento);
+        if(id<=0){
+            throw new IllegalArgumentException("O número do canditado não pode ser nulo ou negativo!");
+        }if(partido ==null || partido.isBlank()){
+            throw new IllegalArgumentException("a filiação partidária deste candidato é obrigatória!!");
+        }
+        this.id = id;
+        this.Partido = Partido;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getPartido() {
+        return Partido;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d - %s (%s)", this.id, getNome(), this.Partido);
+    }
 
 }
