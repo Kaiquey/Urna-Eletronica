92274cfaaa8aa8ebb648205c973a6ba2dd2bd148
Kaique.Dev <kaiquesouzacosta16@gmail.com>
2026-09-11 17:24:01 -0300

    Create Getters and setters, update parameters constructor

--- Urna/src/Eleitor.java
+++ Urna/src/Eleitor.java
@@ -4,11 +4,21 @@ public class Eleitor extends Pessoa{
     private String tituloEleitor;
     private boolean voto;
 
-    public Eleitor(String nome, String cpf, Localdate dataNascimento, String tituloEleitor){
+    public Eleitor(String nome, String cpf, LocalDate dataNascimento, String tituloEleitor){
         super(nome, cpf, dataNascimento);
         this.tituloEleitor = tituloEleitor;
         this.voto = false;
     }
 
+    public String getTituloEleitor() {
+        return tituloEleitor;
+    }
+    public void setTituloEleitor(String tituloEleitor) {
+        this.tituloEleitor = tituloEleitor;
+    }
+    public boolean isVoto() {
+        return voto;
+    }
+    public void setVoto(boolean voto) {}
 
 }
